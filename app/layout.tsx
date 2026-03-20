6408740ca50db34fdda8e04334574c5e6f478253
Li Zhang Tam <ltam0127@uni.sydney.edu.au>
2026-03-20 21:20:48 +1100

    Change font style

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 
 const plusJakarta = Plus_Jakarta_Sans({ 
   subsets: ["latin"],
-  variable: "--font-sans",
+  variable: "--font-plus-jakarta",
 });
 
 export const metadata: Metadata = {
